17e72b0642fa0fea8b6b34fe02902ec4d09b6f5b
Ic3Sandy <monkey_king2@hotmail.com>
2023-02-27 16:11:57 +0700

    🚧 Add factory for deploy singleton, proxy

---
+++ lib/factory.ts
@@ -0,0 +1,81 @@
+import { Client, Contract, MnemonicAccount, PrivkeyAccount } from "firovm-sdk";
+import { ProxyABI, SafeABI } from "./data/abi";
+import { SafeByteCode, SafeProxyFactoryByteCode } from "./data/bytecode";
+import { TxOptions } from "./utils";
+
+export class Factory {
+  private singletonTxId: string;
+  private proxyTxId: string;
+
+  constructor(
+    private client: Client,
+    private account: MnemonicAccount | PrivkeyAccount,
+    private singletonAddress: string = "",
+    private proxyAddress: string = ""
+  ) {
+    this.singletonTxId = "";
+    this.proxyTxId = "";
+  }
+
+  async deploySingleton(options: TxOptions): Promise<string> {
+    if (this.singletonTxId !== "") {
+      return this.singletonTxId;
+    }
+
+    const contract = new this.client.Contract(SafeABI);
+    this.singletonTxId = await contract.deploy(SafeByteCode).send({
+      from: this.account,
+      ...options,
+    });
+    return this.singletonTxId;
+  }
+
+  async deployProxy(options: TxOptions): Promise<string> {
+    if (this.proxyTxId !== "") {
+      return this.proxyTxId;
+    }
+
+    const contract = new this.client.Contract(ProxyABI);
+    this.proxyTxId = await contract.deploy(SafeProxyFactoryByteCode).send({
+      from: this.account,
+      ...options,
+    });
+    return this.proxyTxId;
+  }
+
+  async addressSingleton(): Promise<string | undefined> {
+    if (this.singletonTxId === "") {
+      throw new Error("Singleton not deployed");
+    }
+    if (this.singletonAddress !== "") {
+      return this.singletonAddress;
+    }
+
+    const { result, error } = await this.client.rpcClient.getTransactionReceipt(
+      this.singletonTxId
+    );
+    if (error) {
+      throw new Error(error.message);
+    }
+    this.singletonAddress = `0x${result[0].contractAddress}`;
+    return this.singletonAddress;
+  }
+
+  async addressProxy(): Promise<string | undefined> {
+    if (this.proxyTxId === "") {
+      throw new Error("Proxy not deployed");
+    }
+    if (this.proxyAddress !== "") {
+      return this.proxyAddress;
+    }
+
+    const { result, error } = await this.client.rpcClient.getTransactionReceipt(
+      this.proxyTxId
+    );
+    if (error) {
+      throw new Error(error.message);
+    }
+    this.proxyAddress = `0x${result[0].contractAddress}`;
+    return this.proxyAddress;
+  }
+}
